ac9ba2ea16718bf5f1b385690e245a23d44030e7
Fedja Jeleskovic <jeleskof@ter.teradyne.com>
2026-01-13 16:43:34 -0500

    cleaned up all the files for the documentation only branch

--- Demo/Demo_CSRA/CustomerExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Csra.Interfaces;
-
-namespace Demo_CSRA {
-    public static class CustomerExtensions {
-        public static void CustomerExtension(this ILib.ISetup.IDc dc, string argument1, int argument2) {
-            // Do what you want
-        }
-    }
-}
